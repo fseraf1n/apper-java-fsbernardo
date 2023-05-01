7bacc1d8bfa7b7c0e00eed35f340335151bc10f1
fseraf1n <francisserafinbernardo@gmail.com>
2023-05-02 00:40:49 +0800

    Added PrimeNumber.java file

---
+++ day2/PrimeNumber.java
@@ -0,0 +1,24 @@
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPrime(233234557));
+    }
+
+    public static Boolean isPrime(int val) {
+        if ( val == 2 || val == 3) {
+            return true;
+        }
+
+        if ( val % 2 == 0 || val <= 1) {
+            return false;
+        }
+
+        int valRoot = (int) Math.sqrt(val);
+
+        for (int i = 3 ; i <= valRoot ; i += 2) {
+            if ( val % i == 0 ) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
